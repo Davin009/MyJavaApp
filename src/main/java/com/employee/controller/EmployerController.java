d7c40902d4f293501c8bcc99b22a54fccde55c08
Davin009 <davington009@gmail.com>
2023-04-14 01:32:53 +0530

    Updated

--- src/main/java/com/employee/controller/EmployerController.java
+++ src/main/java/com/employee/controller/EmployerController.java
@@ -1,5 +1,6 @@
 package com.employee.controller;
 
+import com.employee.exception.DataNotFoundException;
 import com.employee.model.EmployeeRepository;
 import com.employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,27 +29,47 @@ public class EmployerController {
     }
 
     @GetMapping("all_employees/{id}")
-    public ResponseEntity<EmployeeRepository> getEmployeeById(@PathVariable("id") Long id) throws Exception {
+    public ResponseEntity<EmployeeRepository> getEmployeeById(@PathVariable("id") Long id) throws DataNotFoundException {
+        try {
             EmployeeRepository entity = employeeServices.getEmployeeById(id);
             return new ResponseEntity<EmployeeRepository>(entity, new HttpHeaders(), HttpStatus.OK);
+        }
+        catch (DataNotFoundException e) {
+            throw new DataNotFoundException("User data Not found in our Record!!");
+        }
     }
     @PostMapping("/employee")
     public ResponseEntity<EmployeeRepository> createOrUpdateEmployee(@RequestBody EmployeeRepository employeeRepository)
-            throws Exception {
-        EmployeeRepository updated = employeeServices.createOrUpdateEmployee(employeeRepository);
-        return new ResponseEntity<EmployeeRepository>(updated, new HttpHeaders(), HttpStatus.OK);
+            throws DataNotFoundException {
+        try {
+            EmployeeRepository updated = employeeServices.createOrUpdateEmployee(employeeRepository);
+            return new ResponseEntity<EmployeeRepository>(updated, new HttpHeaders(), HttpStatus.OK);
+        }
+         catch (DataNotFoundException e) {
+            throw new DataNotFoundException("User data Not found in our Record!!");
+        }
 
     }
     @PutMapping("/employee")
-    public ResponseEntity<EmployeeRepository> updateEmployee(@RequestBody EmployeeRepository employeeRepository) throws Exception {
+    public ResponseEntity<EmployeeRepository> updateEmployee(@RequestBody EmployeeRepository employeeRepository) throws DataNotFoundException {
+        try {
             EmployeeRepository updatedEmployee = employeeServices.updateEmployee(employeeRepository);
             return new ResponseEntity<>(updatedEmployee, new HttpHeaders(), HttpStatus.OK);
+        }
+        catch(DataNotFoundException e) {
+            throw new DataNotFoundException("User data Not found in our Record!!");
+        }
     }
 
-
+    @PreAuthorize("HasRole('EMPLOYER')")
     @DeleteMapping("/{id}")
-    public String deleteEmployeeByEmpId(@PathVariable("id") Long empId) {
+    public String deleteEmployeeByEmpId(@PathVariable("id") Long empId) throws DataNotFoundException {
+        try {
             employeeServices.deleteEmployeeByEmpId(empId);
             return "Deleted Record";
+        }
+        catch (DataNotFoundException e){
+            throw new DataNotFoundException("User data Not found in our Record!!");
+        }
     }
 }
